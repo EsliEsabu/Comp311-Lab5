79080c1596d91351ab351788014b495d37507def
Esli Esabu <EE24019733@BIUST.AC.BW>
2026-09-20 18:58:34 +0200

    Add Library

--- Library.java
+++ Library.java
@@ -1,6 +1,18 @@
-/**
- * Library
- */
-public class Library {
+import java.util.ArrayList;
 
+public class Library {
+    // a list that can only hold Book objects
+    private ArrayList<Book> books = new ArrayList<>();
+
+    // add one book to the list
+    public void addBook(Book b) {
+        books.add(b);
+    }
+
+    // print every book in the list
+    public void printAllBooks() {
+        for (int i = 0; i < books.size(); i++) {
+            System.out.println(books.get(i).getSummary());
+        }
+    }
 }
